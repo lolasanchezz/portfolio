54535e7a581f9459e3960f457e680460902548b0
lolasanchezz <lolansanchez@icloud.com>
2026-01-10 03:07:34 -0500

    x button added, first part of projects added

---
+++ src/app/projects/indProject.tsx
@@ -0,0 +1,39 @@
+import styles from './indProject.module.css'
+
+const Header = (props: {name: string, gh: string, demo: string, backFunc: any}) => {
+    return (
+        <div className = {styles.headerCont}>
+            <p onClick = {() => {props.backFunc(null)}}>x</p>
+        <div className = {styles.header}>    
+            <h1 style = {{textDecoration: 'underline'}}>{props.name}</h1>
+            <div className = {styles.linkCont}>
+                <p>links</p>
+            <div className = {styles.smallLinkCont}>
+                <a href={props.gh}>github</a>
+                <a href={props.demo}>demo</a>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+
+
+
+export const SecProject = (props: {goBackFunc: any}) => {
+    console.log(props.goBackFunc)
+    return (
+        <div className = {styles.projContainer}>
+           <Header backFunc = {props.goBackFunc} name="graph the sec" gh="link" demo="link"/>
+        </div>
+    )
+}
+
+
+export const SecondProject = () => {
+    return (
+<div></div>    )
+}
+
+
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,32 +1,68 @@
 "use client";
 import styles from "./projects.module.css";
 import Sphere from "../three-scrips/Sphere";
-import { useState } from "react";
+import { ReactElement, useState } from "react";
+import { JSX } from "react/jsx-runtime";
+import { SecProject, SecondProject } from "./indProject";
 
 export default function Projects() {
   const base = {
     color: "black",
     textDecoration: "none",
   };
-  const hoverSty = { color: "blue", textDecoration: "underline", cursor: "pointer"};
-  const [hoverStyle, setHover] = useState([base, base]);
+  const hoverSty = {
+    color: "blue",
+    textDecoration: "underline",
+    cursor: "pointer",
+  };
+  const [hoverStyle, setHover] = useState([base, base, base, base, base, base]);
   const setHoverStyle = (i: number, hover: boolean) => {
     let tmp = [...hoverStyle];
     if (hover) {
-      tmp[i] = hoverSty
+      tmp[i] = hoverSty;
       console.log("hover");
     } else {
-      tmp[i] = base
+      tmp[i] = base;
       console.log("no hover");
     }
     setHover(tmp);
   };
+
+  const [visibleProj, setProj] = useState<JSX.Element | null>(null);
+
+  const Project = (props: {
+  index: number;
+  projName: string;
+  desc: string;
+  component: () => JSX.Element
+  setProject: any;
+}) => {
+  return (
+    <div
+      onMouseEnter={() => setHoverStyle(props.index, true)}
+      onMouseLeave={() => setHoverStyle(props.index, false)}
+      className={styles.projCont}
+      onClick={() => setProj(props.component)}
+    >
+      <h2 style={hoverStyle[props.index]}> • {props.projName}</h2>
+      <p style={hoverStyle[props.index]}>{props.desc}</p>
+    </div>
+  );
+};
+
+
+
+
+
+
   return (
     <div className={styles.page}>
+       
       <div className={styles.header}>
         <h1>projects</h1>
         <Sphere width={75} height={75} />
       </div>
+       {visibleProj}
       <div className={styles.favoritesCont}>
         <h4>favorites</h4>
         <div
@@ -48,6 +84,13 @@ export default function Projects() {
           <p style={hoverStyle[1]}>encrypted journal in the terminal</p>
         </div>
         <hr></hr>
+        <Project
+          index={2}
+          projName="graph the sec"
+          desc="security and exchange commission data graphed"
+  component={() => <SecProject goBackFunc={setProj}/>}  // Pass as function
+          setProject={setProj}
+        />
       </div>
     </div>
   );
